9b800474ace61dd6d41c0eedc5016b0c6381dcda
lawden <lawtaden@gmail.com>
2024-07-23 19:55:42 +0200

    fifo cache

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -22,6 +22,7 @@ class FIFOCache(BaseCaching):
             elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 first_key = self.order.pop(0)
                 del self.cache_data[first_key]
+                print("DISCARD:", first_key)
             self.cache_data[key] = item
             self.order.append(key)
 
