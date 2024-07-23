54385c8911e989ce53415719531c3a83f3433c8d
lawden <lawtaden@gmail.com>
2024-07-23 19:41:50 +0200

    FIFO caching

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+FIFOCache module
+"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """ FIFOCache class """
+
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is not None and item is not None:
+            if key in self.cache_data:
+                self.order.remove(key)
+            elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                first_key = self.order.pop(0)
+                del self.cache_data[first_key]
+            self.cache_data[key] = item
+            self.order.append(key)
+
+    def get(self, key):
+        """ Get an item by key """
+        return self.cache_data.get(key, None)
