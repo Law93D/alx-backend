96951c55c2410552b88d7856b4568d0ef8aec1db
lawden <lawtaden@gmail.com>
2024-07-23 20:46:04 +0200

    mru cache

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+""" 4-mru_cache module """
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """ MRUCache class that inherits from BaseCaching """
+
+    def __init__(self):
+        """ Initialize the cache """
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """ Add an item to the cache """
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            # If the cache is full, remove the most recently used item
+            mru_key = self.order.pop()
+            del self.cache_data[mru_key]
+            print(f"DISCARD: {mru_key}")
+
+        # Add the new item to the cache and update the order
+        self.cache_data[key] = item
+        self.order.append(key)
+
+    def get(self, key):
+        """ Retrieve an item from the cache """
+        if key is None or key not in self.cache_data:
+            return None
+
+        # Update the order to reflect that the key was recently used
+        self.order.remove(key)
+        self.order.append(key)
+        return self.cache_data[key]
+
+    def print_cache(self):
+        """ Print the current cache """
+        print("Current cache:")
+        for key in self.cache_data:
+            print(f"{key}: {self.cache_data[key]}")
